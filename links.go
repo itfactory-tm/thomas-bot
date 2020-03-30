f42355391365bbf9f07deb2aa3b01ea1b3cf7940
Alex-Coulon <r0781222@student.thomasmore.be>
2020-03-30 20:57:41 +0200

    Website, Rooster, FB, Canvas, ECTS, Lunch, 365, Corona, Stuvo & Discord links toegevoegd

--- links.go
+++ links.go
@@ -6,8 +6,57 @@ import (
 
 func init() {
 	registerCommand("website", sayWebsite)
+	registerCommand("rooster", sayRooster)
+	registerCommand("fb", SayFb)
+	registerCommand("Canvas",sayCanvas)
+	registerCommand("ects", sayEcts)
+	registerCommand("lunch", sayLunch)
+	registerCommand("365", say365)
+	registerCommand("corona", sayCorona)
+	registerCommand("stuvo", sayStuvo)
+	registerCommand("discord", sayDiscord)
 }
 
 func sayWebsite(s *discordgo.Session, m *discordgo.MessageCreate) {
 	s.ChannelMessageSend(m.ChannelID, "Bezoek onze website: https://thomasmore.be/opleidingen/professionele-bachelor/it-factory")
 }
+
+func sayRooster(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "Bekijk hier je lessenrooster: https://rooster.thomasmore.be/")
+}
+
+func SayFb(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "Bekijk hier onze facebook pagina van Toegepaste informatica: https://www.facebook.com/ToegepasteInformatica.ThomasMoreBE & ELO-ICT: https://www.facebook.com/ElektronicaICT.ThomasMoreBE & ACS: https://www.facebook.com/ACS.ThomasMoreBE")
+}
+
+func sayCanvas(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "Bekijk hier je leerplatform (Canvas): https://thomasmore.instructure.com/")
+}
+
+func sayEcts(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "Bekijk hier de ECTS fiches van ELO-ICT: http://onderwijsaanbodkempen.thomasmore.be/2019/opleidingen/n/SC_51260633.html & Toegepaste Informatica: http://onderwijsaanbodkempen.thomasmore.be/opleidingen/n/SC_51260641.html")
+}
+
+func sayLunch(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "Heb je honger? Bekijk hier de menu voor deze week: https://thomasmore365.sharepoint.com/sites/James/NL/stuvo/Paginas/Weekmenu.aspx?tmbaseCampus=Geel")
+}
+
+func Say365(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "Bekijk hier de 365 sharepoint van de ITFactory: https://thomasmore365.sharepoint.com/sites/s.itfactory/SitePages/Start.aspx")
+
+}
+
+func sayCorona(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "Zit je met vragen hoe thomasmore omgaat met corona? Bekijk dan zeker deze pagina: https://thomasmore365.sharepoint.com/sites/James/NL/stuvo/Paginas/Corona.aspx?tmbaseCampus=Geel")
+}
+
+func sayStuvo(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "Heb je nood aan een goed gesprek? Neem dan zeker contact op met Stuvo: https://thomasmore365.sharepoint.com/sites/James/NL/stuvo/Paginas/Nood-aan-een-goed-gesprek.aspx?tmbaseCampus=Geel")
+}
+
+func sayDiscord(s *discordgo.Session, m *discordgo.MessageCreate) {
+	s.ChannelMessageSend(m.ChannelID, "Nog een beetje in de war over hoe Discord werkt?: https://support.discordapp.com/hc/nl")
+}
+
+
+
