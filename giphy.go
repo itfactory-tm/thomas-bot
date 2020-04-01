015ea67e607c89db2baee89499353538087e5476
Maartje Eyskens <maartje@eyskens.me>
2020-04-01 18:56:48 +0200

    MORE GIFS
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- giphy.go
+++ giphy.go
@@ -9,15 +9,39 @@ import (
 
 func init() {
 	registerCommand("clap", "fun", "Applaus!", clap)
+	registerCommand("hug", "fun", "Omdat je altijd een knuffel kunt gebruiken", hug)
+	registerCommand("cat", "fun", "Voor de kattenmensen", cat)
+	registerCommand("dog", "fun", "Voor de honden fans", dog)
+	registerCommand("bunny", "fun", "Voor de niet katten of hondenmensen", bunny)
 }
 
 func clap(s *discordgo.Session, m *discordgo.MessageCreate) {
+	postRandomGif(s, m, "applause")
+}
+
+func hug(s *discordgo.Session, m *discordgo.MessageCreate) {
+	postRandomGif(s, m, "hug")
+}
+
+func cat(s *discordgo.Session, m *discordgo.MessageCreate) {
+	postRandomGif(s, m, "cat")
+}
+
+func dog(s *discordgo.Session, m *discordgo.MessageCreate) {
+	postRandomGif(s, m, "dog")
+}
+
+func bunny(s *discordgo.Session, m *discordgo.MessageCreate) {
+	postRandomGif(s, m, "bunny")
+}
+
+func postRandomGif(s *discordgo.Session, m *discordgo.MessageCreate, subject string) {
 	if c.GiphyKey == "" {
 		s.ChannelMessageSend(m.ChannelID, "Giphy key is lacking from deployment")
 		return
 	}
 	giphy := libgiphy.NewGiphy(c.GiphyKey)
-	data, err := giphy.GetRandom("applause")
+	data, err := giphy.GetRandom(subject)
 	if err != nil {
 		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Error: %v", err))
 		return
