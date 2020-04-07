f9664010bc506a05b87916ff97f60b5732a9c7df
Maartje Eyskens <maartje@eyskens.me>
2020-04-07 22:08:30 +0200

    Add live tweets from #ITFactory
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- go.mod
+++ go.mod
@@ -4,8 +4,13 @@ go 1.14
 
 require (
 	github.com/bwmarrin/discordgo v0.20.2
+	github.com/davecgh/go-spew v1.1.1
+	github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f
+	github.com/dghubble/oauth1 v0.6.0
 	github.com/go-audio/wav v1.0.0
 	github.com/hraban/opus v0.0.0-20191117073431-57179dff69a6
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/sanzaru/go-giphy v0.0.0-20180211202227-c353d5ec6ee8
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
 )
---
+++ twitter.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"log"
+
+	"github.com/davecgh/go-spew/spew"
+
+	"github.com/dghubble/oauth1"
+
+	"github.com/dghubble/go-twitter/twitter"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+const whatsupChannel = "697150309482496082"
+
+func postHashtagTweets(s *discordgo.Session) {
+	if !c.TwitterEnabled {
+		log.Println("Twitter posting disabled")
+		return
+	}
+
+	config := oauth1.NewConfig(c.TwitterConsumerKey, c.TwitterConsumerSecret)
+	token := oauth1.NewToken(c.TwitterAccessToken, c.TwitterAccessTokenSecret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	params := &twitter.StreamFilterParams{
+		Track:         []string{"#ITFactory"},
+		StallWarnings: twitter.Bool(true),
+	}
+	stream, err := client.Streams.Filter(params)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		if tweet.Retweeted {
+			return
+		}
+		//spew.Dump(tweet)
+		embed := NewEmbed()
+		spew.Dump(tweet.Text)
+		embed.AddField("Tweet", tweet.Text)
+
+		images := []string{}
+
+		if tweet.Entities != nil {
+			for _, media := range tweet.Entities.Media {
+				if media.Type == "photo" {
+					images = append(images, media.MediaURLHttps)
+				}
+			}
+		}
+		if tweet.ExtendedEntities != nil {
+			for _, media := range tweet.ExtendedEntities.Media {
+				if media.Type == "photo" || media.Type == "animated_gif" {
+					images = append(images, media.MediaURLHttps)
+				}
+			}
+		}
+		if len(images) > 0 {
+			embed.SetImage(images[0]) // we can only set 1
+		}
+		embed.SetTitle("@" + tweet.User.ScreenName + ": " + tweet.User.Name)
+		embed.SetURL("https://twitter.com/" + tweet.User.ScreenName + "/status/" + tweet.IDStr)
+		embed.SetThumbnail(tweet.User.ProfileImageURLHttps)
+
+		_, err := s.ChannelMessageSendEmbed(whatsupChannel, embed.MessageEmbed)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	demux.HandleChan(stream.Messages)
+}
