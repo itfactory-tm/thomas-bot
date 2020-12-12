42a244766a4b0283caecb96a4526fb33ebfef729
Maartje Eyskens <maartje@eyskens.me>
2020-12-12 10:49:59 +0000

    fix hive.go
    Signed-off-by: GitHub <noreply@github.com>

--- pkg/commands/hive/hive.go
+++ pkg/commands/hive/hive.go
@@ -9,8 +9,8 @@ import (
 	"github.com/itfactory-tm/thomas-bot/pkg/command"
 )
 
-var channelToCategpry = map[string]string {
-	"775453791801049119" : "775436992136871957" // the hive
+var channelToCategory = map[string]string{
+	"775453791801049119": "775436992136871957", // the hive
 }
 
 var requestRegex = regexp.MustCompile(`!hive ([a-zA-Z0-9-_]*) (.*)`)
@@ -32,7 +32,7 @@ func (h *HiveCommand) Register(registry command.Registry, server command.Server)
 func (h *HiveCommand) SayHive(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	// check of in the request channel to apply limits
-	catId, ok := channelToCategory[m.ChannelID]
+	catID, ok := channelToCategory[m.ChannelID]
 	if !ok {
 		s.ChannelMessageSend(m.ChannelID, "This command only works in the Requests channels")
 		return
@@ -61,7 +61,7 @@ func (h *HiveCommand) SayHive(s *discordgo.Session, m *discordgo.MessageCreate) 
 		Name:      matched[1],
 		Bitrate:   128000,
 		NSFW:      false,
-		ParentID:  catId,
+		ParentID:  catID,
 		Type:      chanType,
 		UserLimit: int(i),
 	})
