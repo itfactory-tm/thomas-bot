018ffd4c0fceedfa1b4a8d13d86db22b8a9fa319
Maartje Eyskens <maartje@eyskens.me>
2021-02-13 20:45:02 +0100

    Make Role Management fully configurable
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- pkg/commands/members/members.go
+++ pkg/commands/members/members.go
@@ -7,16 +7,10 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/itfactory-tm/thomas-bot/pkg/db"
-	"github.com/itfactory-tm/thomas-bot/pkg/embed"
-
 	"github.com/bwmarrin/discordgo"
 	"github.com/itfactory-tm/thomas-bot/pkg/command"
+	"github.com/itfactory-tm/thomas-bot/pkg/db"
 )
-
-// TODO: replace me
-const itfDiscord = "687565213943332875"
-const guestRole = "687568536356257890"
 
 // MemberCommands contains the tm!role command and welcome messages
 type MemberCommands struct {
@@ -82,45 +76,31 @@ func (m *MemberCommands) onGuildMemberAdd(s *discordgo.Session, g *discordgo.Gui
 		}
 	}()
 
-	if g.GuildID == itfDiscord {
-		m.superITFSpecificStuffWeShouldPutIntoAGeneralThing(s, g)
+	if conf.RoleManagement.DefaultRole != "" {
+		err := s.GuildMemberRoleAdd(g.GuildID, g.Member.User.ID, conf.RoleManagement.DefaultRole)
+		if err != nil {
+			log.Printf("Cannot set role for user %s: %q\n", g.Member.User.ID, err)
+		}
 	}
 
-}
+	if len(conf.WelcomeDM) > 0 {
+		c, err := s.UserChannelCreate(g.Member.User.ID)
+		if err != nil {
+			log.Printf("Cannot DM user %s\n", g.Member.User.ID)
+			return
+		}
 
-func (m *MemberCommands) superITFSpecificStuffWeShouldPutIntoAGeneralThing(s *discordgo.Session, g *discordgo.GuildMemberAdd) {
-	err := s.GuildMemberRoleAdd(g.GuildID, g.Member.User.ID, guestRole) // gast role
-	if err != nil {
-		log.Printf("Cannot set role for user %s: %q\n", g.Member.User.ID, err)
+		s.ChannelMessageSend(c.ID, fmt.Sprintf("Hi %s", g.User.Username))
+		time.Sleep(time.Second)
+
+		for _, msg := range conf.WelcomeDM {
+			s.ChannelMessageSend(c.ID, msg)
+			time.Sleep(time.Second)
+		}
+
+		m.SendRoleDM(s, g.GuildID, g.Member.User.ID)
 	}
 
-	c, err := s.UserChannelCreate(g.Member.User.ID)
-	if err != nil {
-		log.Printf("Cannot DM user %s\n", g.Member.User.ID)
-		return
-	}
-
-	s.ChannelMessageSend(c.ID, fmt.Sprintf("Hello %s", g.User.Username))
-	time.Sleep(time.Second)
-	s.ChannelMessageSend(c.ID, "Welcome to the ITFactory Discord!")
-	time.Sleep(time.Second)
-	s.ChannelMessageSend(c.ID, "My name is Thomas Bot, I am a bot who can help you!")
-	time.Sleep(time.Second)
-	s.ChannelMessageSend(c.ID, "New to Discord? No problem we got a manual for you: https://itf.to/discord-help")
-	embed := embed.NewEmbed()
-	embed.SetImage("https://static.eyskens.me/thomas-bot/opendeurdag-1.png")
-	embed.SetURL("https://itf.to/discord-help")
-	s.ChannelMessageSendEmbed(c.ID, embed.MessageEmbed)
-
-	time.Sleep(time.Second)
-	s.ChannelMessageSend(c.ID, "If you need help just type tm!help")
-	time.Sleep(time.Second)
-	s.ChannelMessageSend(c.ID, "Warning, i am only able to reply to messages starting with `tm!`, not to normal questions.")
-	time.Sleep(5 * time.Second)
-	s.ChannelMessageSend(c.ID, "Please set your name for our Discord server to your actual name, this will help us to identify you and let you in! Thank you!")
-	time.Sleep(3 * time.Second)
-
-	m.SendRoleDM(s, g.Member.User.ID)
 }
 
 // Info return the commands in this package
--- pkg/db/config.go
+++ pkg/db/config.go
@@ -1,9 +1,13 @@
 package db
 
 type Configuration struct {
-	WelcomeChannelID string              `json:"welcomeChannelID"`
-	WelcomeText      string              `json:"welcomeText"`
-	Hives            []HiveConfiguration `json:"hives"`
+	WelcomeChannelID string   `json:"welcomeChannelID"`
+	WelcomeText      string   `json:"welcomeText"`
+	WelcomeDM        []string `json:"welcomeDM"`
+
+	RoleManagement RoleManagementConfiguration `json:"roleManagement"`
+
+	Hives []HiveConfiguration `json:"hives"`
 }
 
 type HiveConfiguration struct {
@@ -13,4 +17,18 @@ type HiveConfiguration struct {
 	VoiceCategoryID    string   `json:"voiceCategoryID"`
 	Prefix             string   `json:"prefix"`
 	VoiceBitrate       int      `json:"voiceBitrate"`
+}
+
+type RoleManagementConfiguration struct {
+	Message            string `json:"message"`
+	RoleAdminChannelID string `json:"roleAdminChannelID"`
+	DefaultRole        string `json:"defaultRole"`
+
+	Roles []Role `json:"roles"`
+}
+
+type Role struct {
+	ID    string `json:"id"`
+	Emoji string `json:"emoji"`
+	//Name  string `json:"name"`
 }
