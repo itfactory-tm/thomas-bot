dbc19b4d569817cf71bf041c8e6f180afc8e9971
Maartje Eyskens <maartje@eyskens.me>
2021-02-13 19:36:55 +0100

    Refactor cleanup for DB
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- cmd/alf/clean.go
+++ cmd/alf/clean.go
@@ -4,8 +4,13 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
+
+	"github.com/itfactory-tm/thomas-bot/pkg/db"
 
 	"github.com/bwmarrin/discordgo"
 
@@ -14,23 +19,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// TODO: automate these
-const itfDiscord = "687565213943332875"
-
-// default channel with prefixes
-var hiveCategories = map[string]string{
-	"775436992136871957": "",     // the hive
-	"760860082241142790": "bob-", // ITF gaming
-}
-
-const junkyard = "780775904082395136"
-
 func init() {
 	rootCmd.AddCommand(NewCleanCmd())
 }
 
 type cleanCmdOptions struct {
 	Token string
+
+	MongoDBURL string `envconfig:"MONGODB_URL"`
+	MongoDBDB  string `envconfig:"MONGODB_DB"`
+	ConfigPath string `default:"./config.json" envconfig:"CONFIG"`
+
+	dg           *discordgo.Session
+	shouldRemove map[string]bool
+	db           db.Database
 }
 
 // NewCleanCmd generates the `clean` command
@@ -64,6 +66,20 @@ func (v *cleanCmdOptions) Validate(cmd *cobra.Command, args []string) error {
 func (v *cleanCmdOptions) RunE(cmd *cobra.Command, args []string) error {
 	log.Println("Starting Alf...")
 
+	var err error
+	if v.MongoDBDB != "" {
+		v.db, err = db.NewMongoDB(v.MongoDBURL, v.MongoDBDB)
+		if err != nil {
+			return err
+		}
+	} else {
+		// local fallback
+		v.db, err = db.NewLocalDB(v.ConfigPath)
+		if err != nil {
+			return err
+		}
+	}
+
 	dg, err := discordgo.New("Bot " + v.Token)
 	if err != nil {
 		return fmt.Errorf("error creating Discord session: %w", err)
@@ -78,61 +94,107 @@ func (v *cleanCmdOptions) RunE(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("error opening Discord session: %w", err)
 	}
 
+	v.dg = dg
+
 	// small in memory structure to keep candidates to remove
-	shouldRemove := map[string]bool{}
+	v.shouldRemove = map[string]bool{}
 
-	for {
-		time.Sleep(300 * time.Second)
+	go func() {
+		for {
+			time.Sleep(300 * time.Second)
 
-		state, err := dg.State.Guild(itfDiscord)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-
-		channels, err := dg.GuildChannels(itfDiscord)
-		if err != nil {
-			return err
-		}
-
-		for _, channel := range channels {
-			if prefix, isInHiveCat := hiveCategories[channel.ParentID]; isInHiveCat && channel.Type == discordgo.ChannelTypeGuildVoice {
-				if prefix != "" {
-					if !strings.HasPrefix(channel.Name, prefix) {
-						continue
-					}
+			guilds, err := dg.UserGuilds(100, "", "")
+			for len(guilds) > 0 {
+				for _, guild := range guilds {
+					log.Println("Checking", guild.Name)
+					v.checkGuild(guild.ID)
 				}
-				log.Println("looking at", channel.Name)
-				inUse := false
-				for _, vs := range state.VoiceStates {
-					if vs.ChannelID == channel.ID {
-						inUse = true
-						log.Println(channel.Name, "is in use")
-						break
-					}
+				guilds, err = dg.UserGuilds(100, "", guilds[len(guilds)-1].ID)
+				if err != nil {
+					break
 				}
-
-				// on first occurance: mark to remove, on second occurance remove
-				if wasMarkedAsRemove := shouldRemove[channel.ID]; wasMarkedAsRemove && !inUse {
-					log.Println("Deleting", channel.ID, channel.Name)
-					j, err := dg.Channel(junkyard)
-					if err != nil {
-						log.Println(err)
-						continue
-					}
-					_, err = dg.ChannelEditComplex(channel.ID, &discordgo.ChannelEdit{
-						ParentID:             junkyard,
-						PermissionOverwrites: j.PermissionOverwrites,
-					})
-					if err != nil {
-						log.Println(err)
-					}
-				}
-
-				shouldRemove[channel.ID] = !inUse
 			}
 		}
+	}()
 
-	}
+	log.Println("Thomas Bot Alf (cleanup) is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
 	return nil
+}
+
+func (v *cleanCmdOptions) checkGuild(guildID string) {
+	state, err := v.dg.State.Guild(guildID)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	channels, err := v.dg.GuildChannels(guildID)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	conf, err := v.db.ConfigForGuild(guildID)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if conf == nil || len(conf.Hives) == 0 {
+		// hive guser
+		return
+	}
+
+	for _, channel := range channels {
+		if conf, isHive, _ := v.getConfigForRequestCategory(conf, channel); isHive && channel.Type == discordgo.ChannelTypeGuildVoice {
+			if conf.Prefix != "" {
+				if !strings.HasPrefix(channel.Name, conf.Prefix) {
+					continue
+				}
+			}
+			log.Println("looking at", channel.Name)
+			inUse := false
+			for _, vs := range state.VoiceStates {
+				if vs.ChannelID == channel.ID {
+					inUse = true
+					log.Println(channel.Name, "is in use")
+					break
+				}
+			}
+
+			// on first occurance: mark to remove, on second occurance remove
+			if wasMarkedAsRemove := v.shouldRemove[channel.ID]; wasMarkedAsRemove && !inUse {
+				log.Println("Deleting", channel.ID, channel.Name)
+				j, err := v.dg.Channel(conf.JunkyardCategoryID)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				_, err = v.dg.ChannelEditComplex(channel.ID, &discordgo.ChannelEdit{
+					ParentID:             conf.JunkyardCategoryID,
+					PermissionOverwrites: j.PermissionOverwrites,
+				})
+				if err != nil {
+					log.Println(err)
+				}
+				delete(v.shouldRemove, channel.ID)
+			}
+
+			v.shouldRemove[channel.ID] = !inUse
+		}
+	}
+}
+
+func (v *cleanCmdOptions) getConfigForRequestCategory(conf *db.Configuration, channel *discordgo.Channel) (*db.HiveConfiguration, bool, error) {
+	for _, hive := range conf.Hives {
+		if channel.ParentID == hive.VoiceCategoryID || channel.ParentID == hive.TextCategoryID {
+			return &hive, true, nil
+		}
+	}
+
+	// no hive found
+	return nil, false, nil
 }
