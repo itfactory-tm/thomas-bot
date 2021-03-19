45a0d21ff091eef972df9d356ec33f9685ba8969
Victonator <victorwelters@gmail.com>
2021-03-19 16:03:20 +0100

    Added filter to membercount

--- pkg/commands/moderation/membercount.go
+++ pkg/commands/moderation/membercount.go
@@ -2,7 +2,9 @@ package moderation
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
+	"strings"
 
 	"github.com/itfactory-tm/thomas-bot/pkg/embed"
 
@@ -41,6 +43,9 @@ func (m *ModerationCommands) membercount(s *discordgo.Session, msg *discordgo.Me
 		}
 	}
 
+	//Get updated rolelist
+	roleList, err := s.GuildRoles(msg.GuildID)
+
 	//Put the roles into a map and count how many users have that role
 	roleMap := make(map[string]int)
 	for _, member := range memberList {
@@ -52,6 +57,20 @@ func (m *ModerationCommands) membercount(s *discordgo.Session, msg *discordgo.Me
 		}
 	}
 
+	//Check if user wants to sort by amount (xx!membercount a) OR position of role (default)
+	if mes := strings.Fields(msg.Message.Content); len(mes) > 1 {
+		if strings.HasPrefix(mes[1] ,"a") {
+			//Sort by amount of people
+			sort.Slice(roleList, func(i, j int) bool {
+				return roleMap[roleList[i].ID] > roleMap[roleList[j].ID]
+			})
+		} else{
+			sortByPosition(roleList)
+		}
+	} else{
+		sortByPosition(roleList)
+	}
+
 	//Create embed
 	embedmessage := embed.NewEmbed()
 	embedmessage.SetTitle("Membercount")
@@ -60,11 +79,11 @@ func (m *ModerationCommands) membercount(s *discordgo.Session, msg *discordgo.Me
 	embedmessage.AddField("Total", strconv.Itoa(g.MemberCount))
 
 	//Print to embed if the role has more than 1 user (filters bot roles)
-	for _, role := range g.Roles {
+	for _, role := range roleList {
 		userCount := roleMap[role.ID]
 		if userCount > 1 {
 			embedmessage.AddField(role.Name, strconv.Itoa(userCount))
-			//Discord embeds only allow 25 fields => make new embed
+			//Discord embeds only allows 25 fields => make new embed
 			if len(embedmessage.Fields) >= 25 {
 				sendEmbed(s, msg, embedmessage)
 				embedmessage = embed.NewEmbed()
@@ -77,6 +96,13 @@ func (m *ModerationCommands) membercount(s *discordgo.Session, msg *discordgo.Me
 	if len(embedmessage.Fields) != 0 {
 		sendEmbed(s, msg, embedmessage)
 	}
+}
+
+func sortByPosition(roleList []*discordgo.Role){
+	//Sort by role position
+	sort.Slice(roleList, func(i, j int) bool {
+		return roleList[i].Position > roleList[j].Position
+	})
 }
 
 func sendEmbed(s *discordgo.Session, msg *discordgo.MessageCreate, embedmessage *embed.Embed) {
