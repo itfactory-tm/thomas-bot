13dd0c8c7bb3b91f27962617c1b020a637632a87
Maartje Eyskens <maartje@eyskens.me>
2021-05-07 13:22:23 +0200

    Improve tm!hive message
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- pkg/commands/hive/old.go
+++ pkg/commands/hive/old.go
@@ -9,11 +9,11 @@ import (
 func (h *HiveCommand) Register(registry command.Registry, server command.Server) {
 	if h.isBob {
 		registry.RegisterMessageCreateHandler("vc", func(session *discordgo.Session, create *discordgo.MessageCreate) {
-			session.ChannelMessageSend(create.ChannelID, "Thank you for your interest in `bob!vc`, due to new Discord capabilities this command has been replaced by `/hive`. We hope you enjoy the new experience!")
+			session.ChannelMessageSend(create.ChannelID, "Thank you for your interest in `bob!vc`, due to new Discord capabilities this command has been replaced by `/hive`. The syntax is different but it has auto complete! We hope you enjoy the new experience!")
 		})
 	} else {
 		registry.RegisterMessageCreateHandler("hive", func(session *discordgo.Session, create *discordgo.MessageCreate) {
-			session.ChannelMessageSend(create.ChannelID, "Thank you for your interest in `tm!hive`, due to new Discord capabilities this command has been replaced by `/hive`. We hope you enjoy the new experience!")
+			session.ChannelMessageSend(create.ChannelID, "Thank you for your interest in `tm!hive`, due to new Discord capabilities this command has been replaced by `/hive`. The syntax is different but it has auto complete! We hope you enjoy the new experience!")
 		})
 		registry.RegisterMessageCreateHandler("attendance", h.SayAttendance)
 		registry.RegisterMessageCreateHandler("verify", h.SayVerify)
