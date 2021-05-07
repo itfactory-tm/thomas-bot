ebfa471945b9568430da7cf429e6109249d094e9
Maartje Eyskens <maartje@eyskens.me>
2021-05-07 14:46:12 +0200

    Make images a slash command
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- pkg/commands/images/images.go
+++ pkg/commands/images/images.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"reflect"
 
 	"github.com/itfactory-tm/thomas-bot/pkg/embed"
 
@@ -11,144 +12,175 @@ import (
 	"github.com/itfactory-tm/thomas-bot/pkg/command"
 )
 
+type imageFunction func(s *discordgo.Session, in *discordgo.InteractionCreate) *discordgo.MessageEmbed
+
 // ImagesCommands contains the tm!hello command
-type ImagesCommands struct{}
+type ImagesCommands struct {
+	images map[string]imageFunction
+}
 
 // NewImagesCommands gives a new ImagesCommands
 func NewImagesCommands() *ImagesCommands {
-	return &ImagesCommands{}
+	i := &ImagesCommands{}
+
+	i.images = map[string]imageFunction{
+		"erasmus":  i.sayErasmus,
+		"partners": i.sayPartners,
+		"loesje":   i.sayLoesje,
+		"geit":     i.sayGeit,
+		"paard":    i.sayPaard,
+		"schaap":   i.saySchaap,
+		"steun":    i.saySteun,
+		"love":     i.sayLove,
+	}
+	return i
 }
 
 // Register registers the handlers
 func (i *ImagesCommands) Register(registry command.Registry, server command.Server) {
-	registry.RegisterMessageCreateHandler("erasmus", i.sayErasmus)
-	registry.RegisterMessageCreateHandler("partners", i.sayPartners)
-	registry.RegisterMessageCreateHandler("love", i.sayLove)
-	registry.RegisterMessageCreateHandler("loesje", i.sayLoesje)
-	registry.RegisterMessageCreateHandler("geit", i.sayGeit)
-	registry.RegisterMessageCreateHandler("paard", i.sayPaard)
-	registry.RegisterMessageCreateHandler("schaap", i.saySchaap)
-	registry.RegisterMessageCreateHandler("steun", i.saySteun)
+	registry.RegisterInteractionCreate("image", i.slashCommand)
 }
 
 // InstallSlashCommands registers the slash commands
 func (i *ImagesCommands) InstallSlashCommands(session *discordgo.Session) error {
-	return nil
+	var choices []*discordgo.ApplicationCommandOptionChoice
+	for name := range i.images {
+		choices = append(choices, &discordgo.ApplicationCommandOptionChoice{
+			Name:  name,
+			Value: name,
+		})
+	}
+
+	app := discordgo.ApplicationCommand{
+		Name:        "image",
+		Description: "Gives an image",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "name",
+				Description: "name of the picture",
+				Required:    true,
+				Choices:     choices,
+			},
+		},
+	}
+
+	cmds, err := session.ApplicationCommands(session.State.User.ID, "")
+	if err != nil {
+		return err
+	}
+	exists := false
+	for _, cmd := range cmds {
+		if cmd.Name == "image" {
+			exists = reflect.DeepEqual(app.Options, cmd.Options)
+		}
+	}
+
+	if !exists {
+		_, err = session.ApplicationCommandCreate(session.State.User.ID, "", &app)
+	}
+
+	return err
 }
 
 // Info return the commands in this package
 func (i *ImagesCommands) Info() []command.Command {
-	return []command.Command{
-		command.Command{
-			Name:        "geit",
-			Category:    command.CategoryFun,
-			Description: "De E-F-blok geiten nu ook online",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "paard",
-			Category:    command.CategoryFun,
-			Description: "De E-F-blok paardjes nu ook online",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "schaap",
-			Category:    command.CategoryFun,
-			Description: "De E-F-blok schapen nu ook online",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "steun",
-			Category:    command.CategoryFun,
-			Description: "Examens? We komen er samen door!",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "erasmus",
-			Category:    command.CategoryLinks,
-			Description: "Link naar erasmus",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "partners",
-			Category:    command.CategoryLinks,
-			Description: "Link naar partners",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "love",
-			Category:    command.CategoryFun,
-			Description: "Toon wat liefde aan elkaar <3",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "loesje",
-			Category:    command.CategoryFun,
-			Description: "'Een fan van loesje' heeft wijze spreuken",
-			Hidden:      false,
-		},
-	}
+	return []command.Command{}
 }
 
-func (i *ImagesCommands) sayErasmus(s *discordgo.Session, m *discordgo.MessageCreate) {
+func (i *ImagesCommands) slashCommand(s *discordgo.Session, in *discordgo.InteractionCreate) {
+	if len(in.Data.Options) > 0 {
+		if key, ok := in.Data.Options[0].Value.(string); ok {
+			if fn, ok := i.images[key]; ok {
+				err := s.InteractionRespond(in.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionApplicationCommandResponseData{
+						Embeds: []*discordgo.MessageEmbed{
+							fn(s, in),
+						},
+					},
+				})
+
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}
+	}
+
+	err := s.InteractionRespond(in.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionApplicationCommandResponseData{
+			Content: "sorry I didn't find that image",
+			Flags:   64,
+		},
+	})
+
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+func (i *ImagesCommands) sayErasmus(s *discordgo.Session, in *discordgo.InteractionCreate) *discordgo.MessageEmbed {
 	e := embed.NewEmbed()
 	e.SetTitle("Erasmus @ ITfactory")
 	e.SetImage("https://static.eyskens.me/thomas-bot/sem_2_2020.gif")
 	e.SetURL("https://thomasmore365.sharepoint.com/sites/james/NL/international?tmbaseCampus=Geel")
-	s.ChannelMessageSendEmbed(m.ChannelID, e.MessageEmbed)
+	return e.MessageEmbed
 }
 
-func (i *ImagesCommands) sayPartners(s *discordgo.Session, m *discordgo.MessageCreate) {
+func (i *ImagesCommands) sayPartners(s *discordgo.Session, in *discordgo.InteractionCreate) *discordgo.MessageEmbed {
 	e := embed.NewEmbed()
 	e.SetTitle("Partners in education")
 	e.SetImage("https://static.eyskens.me/thomas-bot/voorstelling_partners_in_education.png")
-	s.ChannelMessageSendEmbed(m.ChannelID, e.MessageEmbed)
+	return e.MessageEmbed
 }
 
-func (i *ImagesCommands) sayLove(s *discordgo.Session, m *discordgo.MessageCreate) {
+func (i *ImagesCommands) sayLove(s *discordgo.Session, in *discordgo.InteractionCreate) *discordgo.MessageEmbed {
 	e := embed.NewEmbed()
-	guild, err := s.Guild(m.GuildID)
+	guild, err := s.Guild(in.GuildID)
 	if err != nil {
 		log.Println(guild)
-		return
+		return nil
 	}
 	e.SetTitle(fmt.Sprintf("<3 %s <3", guild.Name))
 	e.SetImage("https://static.eyskens.me/thomas-bot/love.gif")
-	s.ChannelMessageSendEmbed(m.ChannelID, e.MessageEmbed)
+	return e.MessageEmbed
 }
 
-func (i *ImagesCommands) sayLoesje(s *discordgo.Session, m *discordgo.MessageCreate) {
+func (i *ImagesCommands) sayLoesje(s *discordgo.Session, in *discordgo.InteractionCreate) *discordgo.MessageEmbed {
 	j := rand.Intn(7)
 	e := embed.NewEmbed()
 	e.SetImage(fmt.Sprintf("https://static.eyskens.me/thomas-bot/loesje%d.png", j+1))
-	s.ChannelMessageSendEmbed(m.ChannelID, e.MessageEmbed)
+	return e.MessageEmbed
 }
 
-func (i *ImagesCommands) sayGeit(s *discordgo.Session, m *discordgo.MessageCreate) {
-	j := rand.Intn(9)
+func (i *ImagesCommands) sayGeit(s *discordgo.Session, in *discordgo.InteractionCreate) *discordgo.MessageEmbed {
+	j := rand.Intn(5)
 	e := embed.NewEmbed()
 	e.SetImage(fmt.Sprintf("https://static.eyskens.me/thomas-bot/geit%d.png", j+1))
-	s.ChannelMessageSendEmbed(m.ChannelID, e.MessageEmbed)
+	return e.MessageEmbed
 }
 
-func (i *ImagesCommands) sayPaard(s *discordgo.Session, m *discordgo.MessageCreate) {
+func (i *ImagesCommands) sayPaard(s *discordgo.Session, in *discordgo.InteractionCreate) *discordgo.MessageEmbed {
 	j := rand.Intn(3)
 	e := embed.NewEmbed()
 	e.SetImage(fmt.Sprintf("https://static.eyskens.me/thomas-bot/paard%d.png", j+1))
-	s.ChannelMessageSendEmbed(m.ChannelID, e.MessageEmbed)
+	return e.MessageEmbed
 }
 
-func (i *ImagesCommands) saySchaap(s *discordgo.Session, m *discordgo.MessageCreate) {
+func (i *ImagesCommands) saySchaap(s *discordgo.Session, in *discordgo.InteractionCreate) *discordgo.MessageEmbed {
 	j := rand.Intn(9)
 	e := embed.NewEmbed()
 	e.SetImage(fmt.Sprintf("https://static.eyskens.me/thomas-bot/schaap%d.png", j+1))
-	s.ChannelMessageSendEmbed(m.ChannelID, e.MessageEmbed)
+	return e.MessageEmbed
 }
 
-func (i *ImagesCommands) saySteun(s *discordgo.Session, m *discordgo.MessageCreate) {
+func (i *ImagesCommands) saySteun(s *discordgo.Session, in *discordgo.InteractionCreate) *discordgo.MessageEmbed {
 	j := rand.Intn(40)
 	e := embed.NewEmbed()
 	e.SetImage(fmt.Sprintf("https://static.eyskens.me/thomas-bot/examensteun/%02d.png", j+1))
 	log.Println(fmt.Sprintf("https://static.eyskens.me/thomas-bot/examensteun/%02d.png", j+1))
-	s.ChannelMessageSendEmbed(m.ChannelID, e.MessageEmbed)
+	return e.MessageEmbed
 }
