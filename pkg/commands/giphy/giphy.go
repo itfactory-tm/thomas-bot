296e8c6045044dabd2bd5d7cea0800acd226bd1d
Maartje Eyskens <maartje@eyskens.me>
2021-05-16 19:50:33 +0200

    Make /gif
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- pkg/commands/giphy/giphy.go
+++ pkg/commands/giphy/giphy.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 
+	"github.com/itfactory-tm/thomas-bot/pkg/util/slash"
+
 	"github.com/itfactory-tm/thomas-bot/pkg/util/voice"
 
 	discordha "github.com/meyskens/discord-ha"
@@ -29,106 +31,144 @@ func NewGiphyCommands() *GiphyCommands {
 
 // Register registers the handlers
 func (g *GiphyCommands) Register(registry command.Registry, server command.Server) {
-	registry.RegisterMessageCreateHandler("clap", g.clap)
-
-	registry.RegisterMessageCreateHandler("hug", func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		g.postRandomGif(s, m, "hug")
-	})
-	registry.RegisterMessageCreateHandler("cat", func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		g.postRandomGif(s, m, "cat")
-	})
-	registry.RegisterMessageCreateHandler("dog", func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		g.postRandomGif(s, m, "dog")
-	})
-	registry.RegisterMessageCreateHandler("bunny", func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		g.postRandomGif(s, m, "bunny")
-	})
-	registry.RegisterMessageCreateHandler("honk", func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		g.postRandomGif(s, m, "untitled goose game")
-	})
+	registry.RegisterInteractionCreate("gif", g.slashCommand)
+	registry.RegisterInteractionCreate("clap", g.clap)
 
 	g.server = server
 }
 
 // InstallSlashCommands registers the slash commands
 func (g *GiphyCommands) InstallSlashCommands(session *discordgo.Session) error {
-	return nil
+	slash.InstallSlashCommand(session, "", discordgo.ApplicationCommand{
+		Name:        "gif",
+		Description: "Posts a GIF",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "name",
+				Description: "name of the GIF",
+				Required:    true,
+				Choices: []*discordgo.ApplicationCommandOptionChoice{
+					{
+						Name:  "hug",
+						Value: "hug",
+					},
+					{
+						Name:  "cat",
+						Value: "cat",
+					},
+					{
+						Name:  "dog",
+						Value: "dog",
+					},
+					{
+						Name:  "bunny",
+						Value: "bunny",
+					},
+					{
+						Name:  "bunny",
+						Value: "bunny",
+					},
+					{
+						Name:  "thumbsup",
+						Value: "thumbsup",
+					},
+				},
+			},
+		},
+	})
+
+	return slash.InstallSlashCommand(session, "", discordgo.ApplicationCommand{
+		Name:        "clap",
+		Description: "Applause!",
+		Options:     []*discordgo.ApplicationCommandOption{},
+	})
 }
 
-func (g *GiphyCommands) clap(s *discordgo.Session, m *discordgo.MessageCreate) {
-	ch, _ := voice.FindVoiceUser(s, m.GuildID, m.Author.ID)
+func (g *GiphyCommands) slashCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	subject := ""
+
+	if len(i.Data.Options) > 0 {
+		if r, ok := i.Data.Options[0].Value.(string); ok {
+			subject = r
+		}
+	}
+
+	if subject == "" {
+		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionApplicationCommandResponseData{
+				Content: "Something went wrong",
+				Flags:   64,
+			},
+		})
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		return
+	}
+
+	g.postRandomGif(s, i, subject)
+}
+
+func (g *GiphyCommands) clap(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	ch, _ := voice.FindVoiceUser(s, i.GuildID, i.Member.User.ID)
 
 	if ch != "" {
 		err := g.server.GetDiscordHA().SendVoiceCommand(discordha.VoiceCommand{
 			ModuleID:  "thomasbot",
-			GuildID:   m.GuildID,
+			GuildID:   i.GuildID,
 			ChannelID: ch,
 			File:      "clappingmono.wav",
-			UserID:    m.Author.ID,
+			UserID:    i.Member.User.ID,
 		})
 		if err != nil {
 			log.Printf("Error sending voice command: %q\n", err)
 		}
 	}
-	g.postRandomGif(s, m, "applause")
+	g.postRandomGif(s, i, "applause")
 }
 
-func (g *GiphyCommands) postRandomGif(s *discordgo.Session, m *discordgo.MessageCreate, subject string) {
+func (g *GiphyCommands) postRandomGif(s *discordgo.Session, i *discordgo.InteractionCreate, subject string) {
 	// TODO: share config with commands
 	if os.Getenv("THOMASBOT_GIPHYKEY") == "" {
-		s.ChannelMessageSend(m.ChannelID, "Giphy key is lacking from deployment")
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionApplicationCommandResponseData{
+				Content: "Giphy key is lacking from deployment",
+				Flags:   64,
+			},
+		})
 		return
 	}
 	giphy := libgiphy.NewGiphy(os.Getenv("THOMASBOT_GIPHYKEY"))
 	data, err := giphy.GetRandom(subject)
 	if err != nil {
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Error: %v", err))
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionApplicationCommandResponseData{
+				Content: fmt.Sprintf("Error: %v", err),
+				Flags:   64,
+			},
+		})
 		return
 	}
 
 	embed := embed.NewEmbed()
 	embed.SetImage(data.Data.Fixed_height_downsampled_url)
-	s.ChannelMessageSendEmbed(m.ChannelID, embed.MessageEmbed)
+
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionApplicationCommandResponseData{
+			Content: "",
+			Embeds:  []*discordgo.MessageEmbed{embed.MessageEmbed},
+		},
+	})
 }
 
 // Info return the commands in this package
 func (g *GiphyCommands) Info() []command.Command {
-	return []command.Command{
-		command.Command{
-			Name:        "clap",
-			Category:    command.CategoryFun,
-			Description: "Applause!",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "hug",
-			Category:    command.CategoryFun,
-			Description: "You can always use a hug",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "cat",
-			Category:    command.CategoryFun,
-			Description: "Purrrrfect",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "dog",
-			Category:    command.CategoryFun,
-			Description: "Not everyone likes cats",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "bunny",
-			Category:    command.CategoryFun,
-			Description: "For those who neither like cats nor dogs",
-			Hidden:      false,
-		},
-		command.Command{
-			Name:        "honk",
-			Category:    command.CategoryFun,
-			Description: "Peace was never an option",
-			Hidden:      false,
-		},
-	}
+	return []command.Command{}
 }
