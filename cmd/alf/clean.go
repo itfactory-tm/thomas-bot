42391bb9ec618550ed00d8112beeac1e6db909d2
Maartje Eyskens <maartje@eyskens.me>
2022-02-20 12:15:27 +0100

    Add more timing

--- cmd/alf/clean.go
+++ cmd/alf/clean.go
@@ -122,6 +122,26 @@ func (v *cleanCmdOptions) RunE(cmd *cobra.Command, args []string) error {
 		}
 	}()
 
+	go func() {
+		for {
+			tz, err := time.LoadLocation("Europe/Brussels")
+			if err != nil {
+				panic(err)
+			}
+
+			dirk, _ := time.Parse("2006-01-02", "2022-06-18")
+			time.Sleep(time.Second)
+			now := time.Now().In(tz)
+			// calculate days till dirk
+			days := dirk.Sub(now).Hours() / 24
+
+			if days <= 100 && days >= 0 && now.Hour() == 0 && now.Minute() == 0 && now.Year() == 2022 {
+				dg.ChannelMessageSend(agora, fmt.Sprintf("@<177531421152247809> you have %d days left of being 39 years old.", int(days)))
+				time.Sleep(2 * time.Minute)
+			}
+		}
+	}()
+
 	// small in memory structure to keep candidates to remove
 	v.shouldRemove = map[string]bool{}
 
